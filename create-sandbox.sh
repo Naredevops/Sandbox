57c6de51fa48ddccfc479bd7d286868e3c633fe3
Naredevops <48598624+Naredevops@users.noreply.github.com>
2024-02-03 13:37:11 +0530

    Update create-sandbox.sh

--- create-sandbox.sh
+++ create-sandbox.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# Define Salesforce CLI alias for the new org
-org_alias="dummy-sandbox"
+# Define Salesforce CLI alias for the new scratch org
+org_alias="dummy-scratch-sandbox"
 
-# Create a new Salesforce Developer Edition org
-sfdx force:org:create -a $org_alias -f config/project-scratch-def.json -s -d 1
+# Create a new Salesforce scratch org
+sfdx force:org:create -a $org_alias -f config/project-scratch-def.json -s -d 7
